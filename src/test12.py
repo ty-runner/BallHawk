c848f9235a56ee760b9a3298205790700bd54f26
ty-runner <ty.runner02@gmail.com>
2024-04-30 01:01:54 -0400

    Update test12.py

--- src/test12.py
+++ src/test12.py
@@ -6,8 +6,9 @@ from deep_sort_realtime.deepsort_tracker import DeepSort
 model = YOLO('yolov8n.pt')
 deepsort = DeepSort(max_age=30)
 
-cap = cv2.VideoCapture('tt1.mp4')
-
+#cap = cv2.VideoCapture('WideWide_-_Clip_027_copy.mp4')
+# cap = cv2.VideoCapture('tt1.mp4')
+cap = cv2.VideoCapture('WideWide_-_Clip_027_copy.mp4')
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
@@ -25,16 +26,23 @@ while cap.isOpened():
                 conf = box.conf.item()
                 cls_id = int(box.cls.item())
 
-                if cls_id == 0 and conf > 0.4:
-                        bbox = [x1, y1, x2, y2]  #xywh
+                if cls_id == 0 and conf > 0.6:
+                        bbox = [x1, y1, x2 - x1, y2 - y1]  #xywh
                         bbox_xywh.append(bbox)
                         confidences.append(conf)
                         class_ids.append(cls_id)
-                        cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 255, 255), 2)
-                        cv2.putText(frame, str(conf), (int(bbox[0]), int(bbox[1] - 10)), 0, 0.75, (0, 255, 0), 2)
     track_info = tuple(zip(bbox_xywh, confidences, class_ids))
-    tracks = deepsort.update_tracks(track_info, frame)
-
+    tracks = deepsort.update_tracks(track_info, frame=frame)
+    track_ids = []
+    if bbox_xywh:
+        for track in tracks:
+            track_id = track.track_id
+            if track_id not in track_ids:
+                ltrb = track.to_ltrb()
+                print(f"ltrb ${ltrb}")
+                cv2.rectangle(frame, (int(ltrb[0]), int(ltrb[1])), (int(ltrb[2]), int(ltrb[3])), (255, 0, 0), 2)
+                track_ids.append(track_id)
+                cv2.putText(frame, str(track_id), (int(ltrb[0]), int(ltrb[1] - 10)), 0, 0.75, (0, 255, 0), 2)
     cv2.imshow('Frame', frame)
 
 
