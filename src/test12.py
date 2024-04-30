c9d5b0149072379d879fb0167fb6e783791c0f85
ty-runner <ty.runner02@gmail.com>
2024-04-29 23:51:11 -0400

    Update test12.py

--- src/test12.py
+++ src/test12.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 from deep_sort_realtime.deepsort_tracker import DeepSort
 #from tracker2 import *
 model = YOLO('yolov8n.pt')
-deepsort = DeepSort(max_age=30, nn_budget=100, override_track_class=None)
+deepsort = DeepSort(max_age=30)
 
 cap = cv2.VideoCapture('tt1.mp4')
 
@@ -25,30 +25,15 @@ while cap.isOpened():
                 conf = box.conf.item()
                 cls_id = int(box.cls.item())
 
-                if cls_id == 0 and conf > 0.6:
-                    bbox = [x1, y1, x2, y2]  #xywh
-                    bbox_xywh.append(bbox)
-                    confidences.append(conf)
-                    class_ids.append(cls_id)
-
-
-    if bbox_xywh:
-        try:
-            for bbox in bbox_xywh:
-                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 255, 255), 2)
-                cv2.putText(frame, str(track_id), (int(bbox[0]), int(bbox[1] - 10)), 0, 0.75, (0, 255, 0), 2)
-            tracks = deepsort.update_tracks(bbox_xywh, confidences, class_ids, frame)
-            # Draw the tracks
-            for track in tracks:
-                if not track.is_confirmed() or track.time_since_update > 1:
-                    continue
-                bbox = track.to_tlbr()
-                track_id = track.track_id
-
-        except Exception as e:
-            print("Error updating tracks:", str(e))
-    else:
-        print("No valid detections.")
+                if cls_id == 0 and conf > 0.4:
+                        bbox = [x1, y1, x2, y2]  #xywh
+                        bbox_xywh.append(bbox)
+                        confidences.append(conf)
+                        class_ids.append(cls_id)
+                        cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 255, 255), 2)
+                        cv2.putText(frame, str(conf), (int(bbox[0]), int(bbox[1] - 10)), 0, 0.75, (0, 255, 0), 2)
+    track_info = tuple(zip(bbox_xywh, confidences, class_ids))
+    tracks = deepsort.update_tracks(track_info, frame)
 
     cv2.imshow('Frame', frame)
 
